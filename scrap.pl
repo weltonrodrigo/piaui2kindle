714beb40fe6ceef41b4b172bf0804f5d07a9587d
Rodrigo Torres <rodrigo@familianascimento.org>
2012-08-01 15:45:37 -0400

    Broke

--- scrap.pl
+++ scrap.pl
@@ -23,70 +23,66 @@ use warnings;
 use Scrappy;
 use Template;
 use IO::File;
-use WWW::Mechanize::Cached;
+use WWW::Mechanize::Cached::GZip;
+use URI;
 
 binmode STDOUT, ":utf8";
 
 my $tt = Template->new({INTERPOLATE => 1, ENCODING => 'utf8'})
-		or die "$Template::ERROR\n";
+	or die "$Template::ERROR\n";
 
 my $io = IO::File->new();
 my $scrappy = Scrappy->new;
-$scrappy->{worker} = new WWW::Mechanize::Cached;
- 
-$scrappy->crawl('http://revistapiaui.estadao.com.br/outras-edicoes/sumario/edicao-64',
-	'/outras-edicoes/sumario/edicao-64' => {
-		'.conteudo li a' => sub {
-			my ($self, $item) = @_;
+$scrappy->{worker} = new WWW::Mechanize::Cached::GZip;
 
+my $base    = 'http://revistapiaui.estadao.com.br';
+my $sumario = URI->new_abs('/outras-edicoes/sumario/edicao-', $base);
+my $atual = 70;
+ 
+# Para guardar informações de todas as edições;
+my @edicoes;
+my $id = 0;
+ 
+# Inicia o crawling pela primeira edição.
+$scrappy->crawl($sumario . 1,
+	'/outras-edicoes/sumario/edicao-:edicao' => {
+		'.conteudo li a' => sub {
+			my ($self, $item, $args) = @_;
+
+			# Alguns sumários contem um link para a próxima edição.
 			return if $item->{href} =~ /proxima-edicao/;
 
-			#print "[ITEM] $item->{text}: $item->{href}\n";
+			# Visitará todos os artigos deste edição e o sumário da próxima,
+			# exceto pela edição atual, que é fechada para assinantes;
 			$self->queue->add($item->{href});
+			$self->queue->add($sumario . $args->{edicao} + 1)
+				unless $args->{edicao} < $atual-1;
 		}
 	},
-	'/edicao-64/:secao/:artigo' => {
-		'p img' => sub {
-			my ($self, $item, $args) = @_;
-
-			return unless $args->{artigo} =~ /mitolo/;
-			
-			my ($dropcap) = $item->{src} =~ m{geral/(.)_*.gif};
-
-			$item->{text} = uc $dropcap; 
-
-		}, 
+	'/:edicao/:secao/:artigo' => {
 		'body' => sub {
 			my ($self, $item, $args) = @_;
-			 
+			
+			my $secao = $args->{secao};
 			my $autor = $self
 				->select('.autor p em')->data->[0]->{text};
+				
 			my $title = $self
-				->select('//div[@class="article"]/div/h3')->data->[0]->{text};
-
-			my @content;
-
-			return unless $args->{artigo} =~ /mitolo/;
-			foreach my $p ( @{$self ->select('.article_content p')->data} ){
-
-				$p->{text} =~ s/[^[:print:]]//g;
-
-				next if $p->{text} =~ /^\s*$/;
-				push @content, $p;
-			}
-
-			my $vars = {
-				title	=> $title, 
-				content	=> \@content,
-			};
-
-			# Arquivo de saida.
-			$io->open("> saida/$args->{artigo}.html");
-			$io->binmode(":utf8");
-
-			$tt->process('templates/artigo.tt', $vars, $io) or die $tt->error();
-
-			$io->close();
+				->select('.article h3')->data->[0]->{text};
+				
+			my $subtitle = $self
+				->select('p.subtitle')->data->[0]->{text};
+				
+			my $date  = $self
+				->select('div.breadcrumbs div.flt-left')->data->[0]->{text};
+			($date) = $date =~ /.*> (.*) $/; 
+			
+			my $article_img = URI->new_abs($self
+				->select('.img_article')->data->[0]->{src}, $base);
+			
+			my $content = join "\n",
+								map $_->{html},
+									@{$self->select('.article_content p')->data};
 			 
 		}
 	}
